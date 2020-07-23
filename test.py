1cc61f397b621470a36e66d54dc6581ec8f1ed54
Delegator-creater <67370686+Delegator-creater@users.noreply.github.com>
2020-07-23 20:20:09 +0500

    Add files via upload
    Добавлена возможность запускать несколько симуляция на несколько процессоров одновременно

--- test.py
+++ test.py
@@ -1,14 +1,32 @@
 from scene import *
+from concurrent.futures import ProcessPoolExecutor
 
+def f1_sin (x):
+    return 0.5 * sin(x)
 
+def f2_sin (x):
+    return 0.2 * sin(x)
 
+def f3_sin (x):
+    return 0.2 * sin(x + pi/4)
+
+def f1_exp (x):
+    return exp(x/50)
+
+def test (file_name : str , sizex : int , sizey : int , function_rate_mutation,\
+                   function_chance_mutation):
+    scena = Scene(sizex , sizey , "")
+    scena.simulation(20 , 20 , 100 , 10000 , function_rate_mutation , function_chance_mutation , file_name)
 
 if __name__ == '__main__':
+    pool = ProcessPoolExecutor(max_workers = 4)
+    a = []
+    a.append(pool.submit(test , 'test1' , 10 , 10 , f1_sin , f2_sin))
+    a.append(pool.submit(test , 'test2' , 10 , 10 , f1_sin , f3_sin))
+    a.append(pool.submit(test , 'test3' , 10 , 10 , f1_exp , f2_sin))
+    a.append(pool.submit(test , 'test4' , 10,  10 , sin    , f1_exp))
 
-    scena = Scene(10 , 10 , "")
+    for i in a:
+        i.result()
 
-    open('data.txt' , 'w')
-    open('data1.txt', 'w')
-
-    scena.simulation(20 , 20 , 100 , 10000 , lambda x : sin(x/50) , lambda x : sin (x/50 + pi/4))
 
